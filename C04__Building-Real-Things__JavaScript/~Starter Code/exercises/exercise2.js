cba94d42bc5fc6a304bb57ecc8a5efa1318ce096
Joshua Smith <jcsmith2393@gmail.com>
2022-02-24 20:29:14 -0600

    c4 exercises1&2

---
+++ C04__Building-Real-Things__JavaScript/~Starter Code/exercises/exercise2.js
@@ -0,0 +1,15 @@
+const array=[true,false,false,true,true,false,false,false]
+const count = array.filter(Boolean).length;
+
+//Function needs to  Create a function that returns the number of true values in an array.
+//needs a loop through the array to find all the true values
+// needs to return that number
+//look at the exercise where we seperated the array into multiple arrays maybe to create arrays that hold the true and false boolean values
+
+
+function seperateArray(array) {
+
+console.log(count);
+
+}
+seperateArray()
--- C04__Building-Real-Things__JavaScript/~Starter Code/js/crazy-buttons.js
+++ C04__Building-Real-Things__JavaScript/~Starter Code/js/crazy-buttons.js
@@ -1,7 +1,14 @@
 // * ========== HTML Element Selectors ========== * \\
-
+const crazyButton = document.querySelector(".crazy-button");
 // * ========== Function Declarations ========== * \\
-// FUNCTION: TSK
+// gets a random posititon 
+function goBtnWild(){
+const offsetTop= Math.random()*(window.innerHeight-crazyButton.clientHeight);
+const offsetLeft= MAth.random()*(window.innerWidth-crazyButton.clientWidth);
 
+crazyButton.style.top=`${offsetTop}px`;
+crazyButton.style.left=`${offseetLeft}px`;
+}
 // * ========== Event Listeners ========== * \\
-// EVENT LISTENER: TSK
+// EVENT LISTENER: listens for the mouse to hover and calls my function
+crazyButton.addEventListener("mouseenter",goBtnWild);
