1acaf52607ddd6e91d8bb13b03204c20774a3c7f
Joshua Smith <jcsmith2393@gmail.com>
2022-02-28 17:26:09 -0600

    Update exercise2.js

--- C04__Building-Real-Things__JavaScript/~Starter Code/exercises/exercise2.js
+++ C04__Building-Real-Things__JavaScript/~Starter Code/exercises/exercise2.js
@@ -1,5 +1,5 @@
-const array=[true,false,false,true,true,false,false,false]
-const count = array.filter(Boolean).length;
+const help=[true,false,false,true,true,false,false,false]
+const count = help.filter(Boolean).length;
 
 //Function needs to  Create a function that returns the number of true values in an array.
 //needs a loop through the array to find all the true values
@@ -7,9 +7,9 @@ const count = array.filter(Boolean).length;
 //look at the exercise where we seperated the array into multiple arrays maybe to create arrays that hold the true and false boolean values
 
 
-function seperateArray(array) {
+function seperateArray() {
 
 console.log(count);
 
 }
-seperateArray()
+seperateArray([help])
